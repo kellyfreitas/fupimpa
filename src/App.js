8032a3f073f8c54d99b366ab5c7384f891a71e13
kellyfreitas <kellyfreitas418@gmail.com>
2021-08-04 18:00:29 -0300

    Rotas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 import './App.css';
+import { CodeEditor } from './components/common/Editor/Editor';
 import { Home } from './components/pages/Home/Home';
+import { PaginaAtividadeX } from './components/pages/PaginaAtividade/PaginaAtividade';
 import { Login } from './components/pages/PaginaLogin/Login';
+import history from './history';
 
-function App() {
+function App() {// '/' é a rpta das atividades/feed 
   return (
-    <BrowserRouter>
-    
+ 
+    <Router history={history}>
+        <Route path="/login">
+            <Login></Login>
+        </Route>
+       
         <Route exact path="/">
             <Home></Home>
         </Route>
 
-        <Route path="/login">
-            <Login></Login>
+        <Route path="/Atividade"
+           component={PaginaAtividadeX}>
         </Route>
-    </BrowserRouter>
+
+        
+    </Router>
     
   );
 }
