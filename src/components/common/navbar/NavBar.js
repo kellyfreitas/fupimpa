1a7fdc4e897a126b2e3e7bc0f4c566c5512268b3
kellyfreitas <kellyfreitas418@gmail.com>
2021-08-04 17:14:53 -0300

    Definição de rotas

--- src/components/common/navbar/NavBar.js
+++ src/components/common/navbar/NavBar.js
@@ -1,8 +1,48 @@
+import { Link, NavLink } from 'react-router-dom';
 import '../../common/colors/colors.css';
 import logo from '../../common/logo.svg';
 import './navbar.css';
 
 
+export function NavBar() {
+
+    return (
+        <div className="navbar background-laranja">
+            <img src={logo} alt="Logo Fupimpa" height="36px"></img>
+            <div className="navbar-container-right">
+                <nav>
+                    <div className="navbar-button">
+                        <NavLink
+                            exact
+                            className="button branco"
+                            to="/">
+                            módulos
+                        </NavLink>
+                    </div>
+                    <div className=" navbar-button">
+                        <NavLink
+                            className="button branco"
+                            to="/turma">
+                            turma
+                        </NavLink>
+                    </div>
+                    <div className="navbar-button">
+                        <NavLink
+                            exact
+                            className="button branco"
+                            to="/perfil">
+                            perfil
+                        </NavLink>
+                    </div>
+
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+
+/*
 export function NavBar(){
     return(
         <div className="navbar background-laranja">
@@ -26,3 +66,4 @@ export function NavButton(props){
         </div>
     );
 }
+*/
